5ad2d82e8fcc4b1aa14d9c6a8bf58795b40a7a95
Christos Psarras <cpsarrac@auth.gr>
2018-01-21 23:45:28 +0200

    Add new tag creator

--- src/presenter/presenter.py
+++ src/presenter/presenter.py
@@ -1,11 +1,13 @@
+from __future__ import division
 from sklearn.metrics.pairwise import cosine_similarity
 
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
 
+import re
+from wordcloud import WordCloud
 
 
 def calc_purity(labels_true, labels_pred, vectorizer, top_terms):
@@ -84,38 +86,104 @@ def calc_topic_categories(top_terms, vectorizer):
     return len(set(kappa))
 
 
-# def create_wordclouds(top_terms, indices):
-#
-#     #  top_terms = [['layout', 'border', 'panel', 'action', 'listener', 'selected'],
-#                  #  ['socket', 'listener', 'open', 'close', 'network', 'open']]
-#     #  top_terms = top_terms[0:8]
-#     #  indices = indices[0:8]
-#     num = len(top_terms[0])
-#
-#     # Transform top term corpus in a weighted top term corpus
-#     weighted_top_terms = []
-#     for topic in top_terms:
-#         weighted_list = []
-#         for ind, term in enumerate(topic):
-#             for i in range(0, num - ind + 1):
-#                 weighted_list.append(term)
-#         weighted_top_terms.append(weighted_list)
-#
-#     for ind, wl in enumerate(weighted_top_terms):
-#         weighted_top_terms[ind] = ' '.join(weighted_top_terms[ind])
-#
-#     for ind, wl in enumerate(weighted_top_terms):
-#         tags = make_tags(get_tag_counts(wl), maxsize=150)
-#         create_tag_image(tags, '../output/topic{}.png'.format(indices[ind]), size=(1300, 1150),
-#                          fontname='PT Sans Regular', layout=2, rectangular=True)
-#     offset = len(top_terms)//3
-#     fig, ax = plt.subplots(3, offset, figsize=(10, 8))
-#     for row in range(3):
-#         for idx in range(offset):
-#             im = mpimg.imread('../output/topic{}.png'.format(indices[idx+row*offset]))
-#             ax[row, idx].imshow(im)
-#             ax[row, idx].set_xticks([])
-#             ax[row, idx].set_yticks([])
-#             ax[row, idx].set_title('Topic #{}'.format(indices[idx+row*offset]))
-#     fig.tight_layout()
-#     fig.savefig('../output/tags.eps', format='eps')
+def create_wordclouds(tag_csv, topic_word_csv):
+
+    random_state = 1
+    pref = '../output/'
+    tag_csv = pref + tag_csv
+
+    topic_word_csv = pref + 'topic_word_' + topic_word_csv
+    use_frequencies = True
+    print_full = False
+    if print_full:
+        img_size = (100, 150)
+        figsize = (20, 10)
+    else:
+        figsize = (7, 3.5)
+        img_size = (int(300 * figsize[0]), int(300 * figsize[1]))
+    # Select the topics to print (only if print full is false
+    selected = []
+
+    top_tags = {}
+    topics = []
+    # with open(".." + os.sep + ".." + os.sep + "topic_tags_5_weka.csv") as infile:
+    with open(tag_csv) as infile:
+        for line in infile:
+            if line:
+                line = line.strip().split(';')
+                top_tags[line[0]] = []
+                topics.append(line[0])
+                if line[1:] is not []:
+                    selected.append(line[0])
+                    for l in line[1:]:
+                        term = l.split('=')
+                        top_tags[line[0]].append(term)
+
+    top_terms_str = {}
+    with open(topic_word_csv) as infile:
+        next(infile)
+        for line in infile:
+            if line:
+                line = line.strip().split(';')
+                top_terms_str[line[0]] = []
+                for l in line[1:]:
+                    print(l)
+                    term = l.split('=')
+                    print(term)
+                    term = (term[0][2:] if term[0].startswith('m_') else term[0], term[1])
+                    top_terms_str[line[0]].append(term)
+
+    top_terms = {}
+    for topic in topics:
+        top_tags[topic] = [(re.sub(r'[^a-zA-Z0-9]', '', t[0]), int(t[1])) for t in top_tags[topic] if t[0] != 'java' and t[0] != 'weka'][:5]
+        top_terms[topic] = [(re.sub(r'[^a-zA-Z0-9]', '', t[0]), float(t[1])) for t in top_terms_str['Topic_' + str(topic)][:5]]
+
+    if print_full:
+        f, axarr = plt.subplots(4, 5, figsize = figsize)
+    else:
+        f, ax = plt.subplots(1, 1, figsize = figsize)
+    for tn, topic in enumerate([topic for topic in topics if print_full or topic in selected]):
+        # print topic
+        # print top_tags[topic]
+        # print top_terms[topic]
+        # print
+        if use_frequencies:
+            top_terms_frequencies = dict([(t[0], t[1] / max(s[1] for s in top_terms[topic])) for t in top_terms[topic]])
+            top_tags_frequencies = dict([(t[0], t[1] / max(s[1] for s in top_tags[topic])) for t in top_tags[topic]])
+            frequencies = top_terms_frequencies
+            frequencies.update(top_tags_frequencies)
+            ccc = lambda word, font_size, position, orientation, font_path, random_state: 'rgb(255, 0, 0)' \
+                if word in [top_tag[0] for top_tag in top_tags[topic]] else 'rgb(0, 0, 255)'
+            wc = WordCloud(background_color="white", prefer_horizontal = 1, stopwords = set(), relative_scaling = 0.4,
+                           min_font_size = 100, max_font_size = 400, color_func = ccc, width = img_size[0],
+                           height = img_size[1], random_state = random_state)
+            wc.generate_from_frequencies(frequencies)
+        else:
+            top_terms_frequencies = dict([(t[0], (5 - i) / 5) for i, t in enumerate(top_terms[topic])])
+            top_tags_frequencies = dict([(t[0], (5 - i) / 5) for i, t in enumerate(top_tags[topic])])
+            frequencies = top_terms_frequencies
+            frequencies.update(top_tags_frequencies)
+            ccc = lambda word, font_size, position, orientation, font_path, random_state: 'rgb(255, 0, 0)' \
+                if word in [top_tag[0] for top_tag in top_tags[topic]] else 'rgb(0, 0, 255)'
+            wc = WordCloud(background_color="white", prefer_horizontal = 1, stopwords = set(), relative_scaling = 0.4,
+                           min_font_size = 100, max_font_size = 400, color_func = ccc, width = img_size[0],
+                           height = img_size[1], random_state = random_state)
+            wc.generate_from_frequencies(frequencies)
+
+        if print_full:
+            axarr[tn % 4, tn // 4].imshow(wc, interpolation="bilinear")
+            axarr[tn % 4, tn // 4].set_title("Topic " + topic)
+            axarr[tn % 4, tn // 4].axis("off")
+        else:
+            ax.imshow(wc, interpolation="bilinear")
+            # ax.set_title("Topic " + topic)
+            ax.axis("off")
+            plt.subplots_adjust(left = 0.03, bottom = 0.03, right = 0.97, top = 0.97)
+            plt.savefig(pref + 'wordcloud_' + topic + '.png', format='png')#, bbox_inches='tight', pad_inches = 0)
+            plt.savefig(pref + 'wordcloud_' + topic + '.eps', format='eps')#, bbox_inches='tight', pad_inches = 0)
+            plt.savefig(pref + 'wordcloud_' + topic + '.pdf', format='pdf')#, bbox_inches='tight', pad_inches = 0)
+
+    if print_full:
+        plt.savefig(pref + 'wordclouds_full.eps', format='eps')
+        plt.savefig(pref + 'wordclouds_full.pdf', format='pdf')
+        plt.show()
